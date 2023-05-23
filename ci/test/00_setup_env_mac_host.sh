b621e1e9abe1e1360e5621aa3af45d29d21e402d
Victor Skovorodnikov <victor3.14@yandex.com>
2023-05-23 11:08:35 -0700

    Modifying boost configure options...

--- ci/test/00_setup_env_mac_host.sh
+++ ci/test/00_setup_env_mac_host.sh
@@ -11,8 +11,8 @@ export PIP_PACKAGES="zmq"
 export GOAL="install"
 export BDB_PREFIX="${BASE_ROOT_DIR}/db5"
 
-export CC="gcc"
-export CFLAGS="-O2 -fvisibility=hidden -std=c89 -pedantic -Wall -Wextra -Wcast-align -Wnested-externs -Wshadow -Wstrict-prototypes -Wundef -Wno-unused-function -Wno-long-long -Wno-overlength-strings -W -g"
+export CC="gcc -ansi -pedantic-errors"
+export CFLAGS="-O2 -fvisibility=hidden -std=c99 -pedantic -Wall -Wextra -Wcast-align -Wnested-externs -Wshadow -Wstrict-prototypes -Wundef -Wno-unused-function -Wno-long-long -Wno-overlength-strings -W -g"
 ##
 # export LDFLAGS="-L/opt/homebrew/Cellar/boost/1.81.0_1/lib"
 # export CPPFLAGS="-I/opt/homebrew/Cellar/boost/1.81.0_1/include"
