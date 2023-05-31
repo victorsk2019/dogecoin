fe2d9c357fb70d84c61f356c0eb01c4aad87fadd
Victor Skovorodnikov <victor3.14@yandex.com>
2023-05-31 08:45:02 -0700

    Updating net_processing.cpp

--- ci/test/00_setup_env_mac_host.sh
+++ ci/test/00_setup_env_mac_host.sh
@@ -13,8 +13,8 @@ export BDB_PREFIX="${BASE_ROOT_DIR}/db5"
 
 # export CFLAGS=""
 ##
-export CXX="gcc"
-export CC="gcc"
+#export CXX="gcc"
+#export CC="gcc"
 export LDFLAGS="-L/opt/homebrew/Cellar/boost/1.81.0_1/lib"
 export CPPFLAGS="-I/opt/homebrew/Cellar/boost/1.81.0_1/include"
 export BITCOIN_CONFIG="--with-gui=no --disable-wallet"
--- src/net_processing.cpp
+++ src/net_processing.cpp
@@ -31,6 +31,7 @@
 #include <util/system.h>
 #include <validation.h>
 
+#include <array>
 #include <memory>
 #include <typeinfo>
 
