f3e3c1edf31f2b748be4d6feffcdd37884f99990
Victor Skovorodnikov <victor3.14@yandex.com>
2023-06-04 11:38:46 -0700

    Modified test-security-check.py

--- contrib/devtools/test-security-check.py
+++ contrib/devtools/test-security-check.py
@@ -67,9 +67,9 @@ class TestSecurityChecks(unittest.TestCase):
         cc = 'clang'
         write_testcode(source)
 
-        self.assertEqual(call_security_check(cc, source, executable, ['-Wl,-no_pie','-fstack-protector-all']),
+        self.assertEqual(call_security_check(cc, source, executable, ['-Wl,-no_pie','-Wl,-flat_namespace','-Wl,-mexecute-only','-fno-stack-protector']),
             (1, executable+': failed PIE NOUNDEFS NX LAZY_BINDINGS Canary'))
-        self.assertEqual(call_security_check(cc, source, executable, ['-Wl,-no_pie','-Wl,-flat_namespace','-Wl,-allow_stack_execute','-fstack-protector-all']),
+        self.assertEqual(call_security_check(cc, source, executable, ['-Wl,-no_pie','-Wl,-flat_namespace','-Wl,-mexecute-only','-fstack-protector-all']),
             (1, executable+': failed PIE NOUNDEFS NX LAZY_BINDINGS'))
         self.assertEqual(call_security_check(cc, source, executable, ['-Wl,-no_pie','-Wl,-flat_namespace','-fstack-protector-all']),
             (1, executable+': failed PIE NOUNDEFS LAZY_BINDINGS'))
